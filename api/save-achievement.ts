ac5838a5331ae10cc06b922be547a8e9777dd4c6
fil1089 <ifilatov2012@gmail.com>
2026-02-28 23:50:13 +0300

    Fix: Add .js extension to ESM import for Vercel deployment

--- api/save-achievement.ts
+++ api/save-achievement.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { sql } from './db';
+import { sql } from './db.js';
 
 export default async function handler(
     request: VercelRequest,
