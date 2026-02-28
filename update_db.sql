159068671c100291e4baafc0e636bf835747d678
fil1089 <ifilatov2012@gmail.com>
2026-03-01 00:38:43 +0300

    Fix: Add username column to update_db.sql

--- update_db.sql
+++ update_db.sql
@@ -16,7 +16,15 @@ BEGIN
   END IF;
 END $$;
 
--- 3. Ensure username is unique
+-- 3. Add username column if it doesn't exist
+DO $$ 
+BEGIN 
+  IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='users' AND column_name='username') THEN
+    ALTER TABLE users ADD COLUMN username TEXT UNIQUE;
+  END IF;
+END $$;
+
+-- 4. Ensure username is unique (as a constraint)
 DO $$ 
 BEGIN 
   IF NOT EXISTS (SELECT 1 FROM information_schema.table_constraints WHERE table_name='users' AND constraint_name='users_username_key') THEN
